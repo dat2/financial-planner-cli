f9055b65562038678d19d00340d245fe46b47a44
Nicholas Dujay <nickdujay@gmail.com>
2017-03-25 17:13:00 -0400

    added deposit rule; restructure input file

--- src/main.rs
+++ src/main.rs
@@ -18,52 +18,61 @@ use std::collections::HashMap;
 
 #[derive(Debug, Deserialize)]
 struct Monthly {
-    name: String,
     amount: f64,
     end_date: Option<DateTime<Local>>
 }
 
 #[derive(Debug, Deserialize)]
 struct BiWeekly {
-    name: String,
     amount: f64,
     end_date: Option<DateTime<Local>>
 }
 
 #[derive(Debug, Deserialize)]
 struct OneTime {
-    name: String,
     amount: f64,
     date: DateTime<Local>
 }
 
 #[derive(Debug, Deserialize)]
-enum IncomeStream {
+enum IncomeSource {
     Monthly(Monthly),
     BiWeekly(BiWeekly),
-    OneTime(OneTime)
+    Once(OneTime)
 }
 
 #[derive(Debug, Deserialize)]
 struct Investment {
-    name: String,
     roi: f64,
     amount: f64
 }
 
 #[derive(Debug, Deserialize)]
-struct Input {
-    income: Vec<IncomeStream>,
-    investments: Vec<Investment>
+struct Deposit {
+    amount: f64,
+    from: String,
+    to: String
 }
 
-fn print_forecast(input: Input, years: u32) {
+#[derive(Debug, Deserialize)]
+enum Rule {
+    Deposit(Deposit)
+}
+
+#[derive(Debug, Deserialize)]
+struct Plan {
+    income: HashMap<String, IncomeSource>,
+    investments: HashMap<String, Investment>,
+    rules: HashMap<String, Rule>
+}
+
+fn print_forecast(input: Plan, years: u32) {
     let mut table = Table::new();
 
     let mut header_row = Vec::new();
     header_row.push(Cell::new("Year"));
-    for investment in &input.investments {
-        header_row.push(Cell::new(&investment.name));
+    for (name, _) in &input.investments {
+        header_row.push(Cell::new(name));
     }
     table.add_row(Row::new(header_row));
 
@@ -73,10 +82,22 @@ fn print_forecast(input: Input, years: u32) {
 
         let mut year_row = Vec::new();
         year_row.push(Cell::new(&year.to_string()));
-        for investment in &input.investments {
-            let mut previous_year_value = previous_year_values.entry(&investment.name).or_insert(investment.amount);
+        for (name, investment) in &input.investments {
+            let mut previous_year_value = previous_year_values.entry(name).or_insert(investment.amount);
             year_row.push(Cell::new(&format!("${:.2}", previous_year_value)));
+
+            // add return
             *previous_year_value *= 1.0 + investment.roi;
+
+            // add rules
+            for (_, rule) in &input.rules {
+                match *rule {
+                    Rule::Deposit(ref d) if d.to == *name => {
+                        *previous_year_value += d.amount;
+                    },
+                    _ => { }
+                }
+            }
         }
         table.add_row(Row::new(year_row));
 
@@ -103,7 +124,7 @@ fn main() {
         .get_matches();
 
     let input_file = File::open(matches.value_of("INPUT").unwrap_or("input.yaml")).unwrap();
-    let input: Input = serde_yaml::from_reader(input_file).unwrap();
+    let input: Plan = serde_yaml::from_reader(input_file).unwrap();
 
     if let Some(matches) = matches.subcommand_matches("forecast") {
         let years = value_t!(matches, "years", u32).unwrap_or(25);
